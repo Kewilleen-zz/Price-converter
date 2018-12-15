ea2eadc5527a941ad055b53ff48018407f77dc2c
KEWILLEEN <kewilleen@outlook.com>
2018-12-15 03:37:27 -0200

    Usando o armazenamento

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+	require 'data/storagemanager.class.php';
 	require 'data/convertor.class.php';
-	$convertor = new Convertor('BRL', 'USD');
+	$storage = new StorageManager();
+	if (!$storage->has('searched')) {
+		echo 'Conversion inserted to session';
+		$storage->set('searched', new Convertor('BRL', 'USD'));
+	}
+	$convertor = $storage->get('searched');
 	echo $convertor->getValue();
 ?>
